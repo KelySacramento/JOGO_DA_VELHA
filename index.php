7c99c7234a46bb70c400fcbbfde8906541bbb619
KelySacramento <122552874+KelySacramento@users.noreply.github.com>
2024-11-06 00:39:31 +0000

    Jogo da velha

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo da Velha</title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+ <body>
+    <h1>Jogo da velha </h1>
+
+    <div id="tabuleiro">
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div> 
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+    <div class="casa"></div>
+</div>
+</body>
+</html>
